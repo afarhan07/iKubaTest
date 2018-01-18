4d6de4362776e8b1d39ba524d3691c5682b71b33
afarhan <ammarotaku@gmail.com>
2018-01-18 20:10:08 +0700

    test

--- app/src/main/java/com/ikuba/ikubatest/JoinActivity.java
+++ app/src/main/java/com/ikuba/ikubatest/JoinActivity.java
@@ -8,18 +8,26 @@ import android.widget.Button;
 
 public class JoinActivity extends AppCompatActivity {
 
-    Button btn_qr;
+    Button btn_joinQR,btn_joinID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
 
-        btn_qr = findViewById(R.id.btn_joinQR);
-        btn_qr.setOnClickListener(new View.OnClickListener() {
+        btn_joinQR = findViewById(R.id.btn_joinQR);
+        btn_joinQR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(JoinActivity.this,JoinQRActivity.class));
+            }
+        });
+
+        btn_joinID = findViewById(R.id.btn_joinID);
+        btn_joinID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
